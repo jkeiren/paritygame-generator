c45d7ad2d07cb968b91f411e7dd2b3e7c914723d
Jeroen Keiren <j.j.a.keiren@gmail.com>
2013-06-12 14:57:11 +0200

    Generate proper box plots from the computed data.
    This properly computes the median, average, and upper- and lower quartiles and whiskers. Also, outliers are displayed in the plot.

--- utilities/plots.py
+++ utilities/plots.py
@@ -126,6 +126,33 @@ def scatterplot(plotcase, conn):
   latexsrc = latexsrc.replace('%maxes', maxes)
   return latexsrc.replace('%values', values)
 
+def scatterplot_mixed(plotcase, conn):
+  print plotcase
+  values = ''
+  cases = plotcase['cases']
+  for case in cases:
+    values += '\n        '.join(
+      ['{0}, {1}, {2}'.format(x, y, case) 
+       for x, y, _ in getplotdata(conn, cases[0], case, plotcase['xval'], plotcase['yval'], plotcase['xmode'], plotcase['ymode'])])
+    values += '\n        '
+  LOG.debug(values)
+  latexsrc = open('templatescatter_mixed.txt').read()
+  marks = ['x', 'o', '+', 'square', 'triangle']
+  classes = []
+  for (index,case) in enumerate(cases):
+    classes.append("{0}={{mark={1}}}".format(case, marks[index]))
+  latexsrc = latexsrc.replace('%classes', ",\n".join(classes))
+  latexsrc = latexsrc.replace('%legend', ", ".join(map(lambda x: '~' + x, plotcase['casenames'] if plotcase.has_key('casename') else cases)))
+  for lbl in ('xmode', 'ymode', 'Xlabel', 'Ylabel'):
+    latexsrc = latexsrc.replace('%' + lbl, str(plotcase[lbl]))
+  maxes = ''
+  if plotcase.has_key('xmax'):
+    maxes = maxes + ",xmax={0}".format(plotcase['xmax'])
+  if plotcase.has_key('ymax'):
+    maxes += ",ymax={0}".format(plotcase['ymax'])
+  latexsrc = latexsrc.replace('%maxes', maxes)
+  return latexsrc.replace('%values', values)
+
 def histogram(plotcase, conn):
   values = '\n        '.join(
     [r'{0:.2f}'.format(100.0 - 100.0 * float(y) / float(x))
@@ -133,7 +160,7 @@ def histogram(plotcase, conn):
   latexsrc = open('templatehist.txt').read()
   return latexsrc.replace('%values', values)
 
-def boxplot(plotcase, conn):
+def boxplot_old(plotcase, conn):
   LOG.debug("Creating boxplot for case {0}".format(plotcase))
   values = [(x, y, cluster) for x, y, cluster in getplotdata(conn, plotcase['xcase'], plotcase['ycase'], plotcase['xval'], plotcase['yval'])]
   LOG.debug("Obtained {0} values".format(len(values)))
@@ -156,6 +183,81 @@ def boxplot(plotcase, conn):
     cases = ""
   return latexsrc.substitute(cases = cases, table = '\n    '.join(coords))
 
+def boxplot(plotcase, conn):
+  LOG.debug("Creating boxplot for case {0}".format(plotcase))
+  values = [(x, y, cluster) for x, y, cluster in getplotdata(conn, plotcase['xcase'], plotcase['ycase'], plotcase['xval'], plotcase['yval'])]
+  LOG.debug("Obtained {0} values".format(len(values)))
+  data = []
+  for cluster in clusters.keys():
+    clustervalues = filter(lambda x: x[2] == cluster, values)
+    if clustervalues == []: continue
+    reductions = map(lambda x: 100.0 - 100.0 * float(x[1]) / float(x[0]), clustervalues)
+    reductions = sorted(reductions)
+    if reductions == []:
+      LOG.warning("No values")
+      continue
+
+    data.append((cluster, reductions))  
+
+# The following computes the information manually, and does not take outliers
+# into account.
+# The version that we actually use does take the outliers into account, and
+# uses a proper computation of the upper and lower whiskers.
+#    size = len(reductions)
+#    avg = sum(reductions) / size
+#    median_pos = int(size // 2) # size divided by 2, floored returns float, cast to int.
+#    lower_whisker = reductions[0]
+#    upper_whisker = reductions[-1]
+#    median = reductions[median_pos]
+#    lower_quartile = reductions[int(median_pos // 2)]
+#    upper_quartile = reductions[median_pos + int((size - median_pos) // 2)]
+#    data.append((cluster, median, upper_quartile, lower_quartile, upper_whisker, lower_whisker, avg))
+#    
+#  boxtemplate = string.Template('''    \\addplot+[
+#    boxplot prepared={
+#      average=${average},
+#      median=${median},
+#      upper quartile=${upper_quartile},
+#      lower quartile=${lower_quartile},
+#      upper whisker=${upper_whisker},
+#      lower whisker=${lower_whisker}
+#    },
+#    ] coordinates {};''')
+
+  boxtemplate = string.Template('''    \\addplot+[
+    boxplot={average=auto}]
+      table[row sep=\\\\,y index=0] {
+        data\\\\
+        ${data}\\\\
+      };''')
+
+  boxes = []
+  yticks = []
+  yticklabels = []
+
+  for index, x in enumerate(data):
+    print x
+    boxes.append(boxtemplate.substitute(data = "\\\\ ".join(map(lambda y: str(y), x[1]))))
+    if plotcase.get('yticklabels', True):
+      yticklabels.append(x[0])
+    yticks.append(str(index+1))
+
+
+#  print data  
+#
+#  boxes = []
+#  yticks = []
+#  yticklabels = []
+#
+#  for index, x in enumerate(data):
+#    boxes.append(boxtemplate.substitute(median = x[1], upper_quartile = x[2], lower_quartile = x[3], upper_whisker = x[4], lower_whisker = x[5], average = x[6]))
+#    if plotcase.get('yticklabels', True):
+#      yticklabels.append(x[0])
+#    yticks.append(str(index+1))
+  
+  latexsrc = string.Template(open('templatebox_new.txt').read())
+  return latexsrc.substitute(yticks = ",".join(yticks), yticklabels= ",".join(yticklabels), boxes = "\n".join(boxes))
+
 def run(plotspec, dbfile):
   cases = yaml.load(open(plotspec).read())
   conn = sqlite3.connect(dbfile)
@@ -166,7 +268,13 @@ def run(plotspec, dbfile):
       continue
     
     if plot['format'] == 'scatterplot':
-      latexsrc = scatterplot(plot, conn)
+      if plot.has_key('xcase'): # Scatterplot simply comparing two dimensions
+        latexsrc = scatterplot(plot, conn)
+      else: # Assume scatterplot with two dimensions, where the plot points are
+            # from different data sets
+        latexsrc = scatterplot_mixed(plot, conn)
+    elif plot['format'] == 'boxplot_old':
+      latexsrc = boxplot(plot, conn)
     elif plot['format'] == 'boxplot':
       latexsrc = boxplot(plot, conn)
     elif plot['format'] == 'histogram':
